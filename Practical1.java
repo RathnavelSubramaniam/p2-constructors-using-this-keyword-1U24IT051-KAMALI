c626234b83a1c61af14989f154841ad4b700ecb1
KAMALI <kamali091106@gmail.com>
2024-12-09 19:15:57 +0530

    Update Practical1.java

--- Practical1.java
+++ Practical1.java
@@ -9,9 +9,7 @@ class Person {
         lastName = "";
         age = 0;
     }
-
-    // Constructor with parameters
-    public Person(String firstName, String lastName, int age) {
+        public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -19,7 +17,7 @@ class Person {
 
     // Method to get full name
     public String getFullName() {
-        return firstName + " " + lastName; // Add space between first and last name
+    return firstName + " " + lastName; // Add space between first and last name
     }
 }
 
@@ -28,12 +26,8 @@ public class Practical1 {
         // Create two Person objects
         Person person1 = new Person("John", "Doe", 30);
         Person person2 = new Person("Alice", "Smith", 25);
-
-        // Print full names of both persons
         System.out.println("Person 1: " + person1.getFullName());
         System.out.println("Person 2: " + person2.getFullName());
-
-        // Calculate and print the average age
         double averageAge = (person1.age + person2.age) / 2.0;
         System.out.println("Average Age: " + averageAge);
     }
